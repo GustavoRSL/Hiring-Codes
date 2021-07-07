afadca873ec1cde85f2d55b6067ff6531b4470de
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-07 15:56:38 -0300

    Adicionando Tipos de dados.

--- JavaScript/Variaveis.js
+++ JavaScript/Variaveis.js
@@ -6,3 +6,32 @@ console.log(myName);
 
 let language = "JavaScript";
 const pattern = "ECMAScript"
+
+// Tipo de variaveis
+var string = "String"
+var inteiro = 1
+var float = 1.2
+
+null
+undefined
+
+true
+false
+
+// Objeto, Vetor e Função
+
+var person = {
+    "name": "GustavoRSL",
+    "age": 20
+}
+
+var estudantes = ["Gustavo" , "R", "S", "L"]
+
+function soma() {
+    let a, b
+    return a + b
+}
+
+// Operador TYPEOF
+
+console.log(typeof Variavel) 
