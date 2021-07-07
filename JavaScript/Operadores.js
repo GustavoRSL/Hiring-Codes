e750b85c6f00355d3a8373843f6c49ab34d407ec
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-07 16:14:03 -0300

    Operadores do JavaScript

---
+++ JavaScript/Operadores.js
@@ -0,0 +1,27 @@
+/* Operadores aritméticos
+
+Adição              +
+Subtração           -
+Multiplicação       *
+Divisão             /
+Modulo/Resto        %
+Incremento          ++
+Decremento          --
+Atribuições         += -= *= /= %=
+
+Operadores de comparação
+==
+!=
+===
+< 
+>
+>=
+<=
+
+Operadores Lógicos
+AND &&
+OR  ||
+NOT !
+
+
+*/
