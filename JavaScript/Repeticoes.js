c888a497402582528e63a39c1249d0f6ad55131b
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-08 13:13:23 -0300

    Repetições em JavaScript

---
+++ JavaScript/Repeticoes.js
@@ -0,0 +1,17 @@
+var cores = ["Amarela","Verde","Azul","Vermelho"];
+
+for(var i = 0; i < cores.length; i++) {
+    console.log(cores[i]);
+}
+
+var num = 0;
+while (num < 10) {
+    console.log(i);
+    i++;
+}
+
+num = 0;
+do {
+    console.log(i);
+    i++;
+}while(i<10);
