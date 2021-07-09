7190ed1d07a9154b697adb5b071bc7642f39510f
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-09 11:47:49 -0300

    Arrow functions em JavaScript

---
+++ JavaScript/ArrowFunctions.js
@@ -0,0 +1,11 @@
+// Function
+function soma(a,b){
+    return a + b;
+}
+
+console.log(soma(3,5));
+
+// Arrow Function
+
+const soma = (num1,num2) => num1 + num2;
+const sayHello = name => `Hello ${name}`;
