7943117e2718cf5d3059dfa586307752058287cb
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-09 11:37:43 -0300

    Operador ternário JavaScript

---
+++ JavaScript/OperadorTernario.js
@@ -0,0 +1,16 @@
+// if ... else
+/*
+if (idade >= 18) {
+    return "Maior de idade."
+}
+else {
+    return "Menor de idade."
+}
+*/
+
+console.log(calculaIdade(18));
+
+function calculaIdade(idade) {
+    return idade >= 18 ? "Maior de idade":"Menor de idade"
+}
+ 
