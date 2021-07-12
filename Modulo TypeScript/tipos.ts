4f182f7b473e7090dab9d5f932988c46d2d95320
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-12 17:58:28 -0300

    Tipos em TypeScript

--- Modulo TypeScript/tipos.ts
+++ Modulo TypeScript/tipos.ts
@@ -54,3 +54,20 @@ criar({
 })
 
 //criar("GustavoRSL");  Dá erro
+
+// Never
+
+function loopInfinito(): never {
+    while(true){
+        // ...
+    }
+}
+
+function erro(mensagem: string): never {
+    throw new Error(mensagem);
+}
+
+function falha(): never {
+    return erro("Algo falhou");
+}
+
