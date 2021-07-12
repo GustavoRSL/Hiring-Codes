6e720816501851e82b4c96c350ac37825df668f9
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-12 17:40:02 -0300

    Tipagem em TypeScript

---
+++ Modulo TypeScript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function somar(num1, num2) {
+    return num1 + num2;
+}
+// Concatenação
+var num1 = 5;
+var num2 = 10;
+console.log(somar(num1, num2));
---
+++ Modulo TypeScript/index.ts
@@ -0,0 +1,9 @@
+function somar (num1: number,num2: number) {
+    return num1 + num2;
+}
+
+// Concatenação
+const num1 = 5;
+const num2 = 10;
+
+console.log(somar(num1,num2));
