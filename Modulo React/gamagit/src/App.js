a0612f9ab27d9573b15a7ac7417fb1c4ea595c69
GustavoRSL <d7gustavo@yahoo.com.br>
2021-07-21 12:59:10 -0300

    Importando Axios

--- Modulo React/gamagit/src/App.js
+++ Modulo React/gamagit/src/App.js
@@ -1,15 +1,23 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
 function App(props) {
-  const [usuario, setUsuario] = useState('GustavoRSL');
+  const [usuario, setUsuario] = useState('');
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response=> console.table(response.data));
+  }
+
+
   return (
     <>
     <h1> { props.title } { props.user } </h1>
     <p> {usuario} </p>
-    <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário" />
-    <button type="button"> Pesquisar </button>
+    <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+    <button type="button" onClick={handlePesquisa}> Pesquisar </button>
     </>
   );
 }
